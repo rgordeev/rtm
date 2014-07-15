206db6db92562de432d5895321de4601223c4302
rgordeev <roma.gordeev@gmail.com>
2014-07-15 17:28:07 +0400

    add jersey rest service

--- src/main/java/config/AppServletContextListener.java
+++ src/main/java/config/AppServletContextListener.java
@@ -4,8 +4,12 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 import controller.RestController;
 import controller.SimpleController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: rgordeev
@@ -14,9 +18,20 @@ import controller.SimpleController;
  */
 public class AppServletContextListener extends GuiceServletContextListener
 {
+    private static final String SERVER_RESOURCES_PACKAGE                = "rest";
+    private static final String JERSEY_API_JSON_POJO_MAPPING_FEATURE    = "com.sun.jersey.api.json.POJOMappingFeature";
+    private static final String JERSEY_CONFIG_PROPERTY_PACKAGES         = "com.sun.jersey.config.property.packages";
+
+
     @Override
     protected Injector getInjector()
     {
+        // инициализируем параметры, которые Guice передаст в параметры инициализации сервлета Jersey
+        final Map<String, String> params = new HashMap<String, String>();
+
+        params.put(JERSEY_CONFIG_PROPERTY_PACKAGES, SERVER_RESOURCES_PACKAGE);
+        params.put(JERSEY_API_JSON_POJO_MAPPING_FEATURE, "true");
+
         // регистрируем Singleton-сервлет SimpleController
         // на все URI вида *.do
         // аннотация @WebServlet("*.do") сервлету более не нужна
@@ -26,8 +41,9 @@ public class AppServletContextListener extends GuiceServletContextListener
                     @Override
                     protected void configureServlets()
                     {
-                        serve("*.do").with(SimpleController.class);
-                        serve("/rest/*").with(RestController.class);
+                        serve("*.do")   .with(SimpleController.class        );
+                        serve("/rst/*") .with(RestController.class          );
+                        serve("/rest/*").with(GuiceContainer.class, params  );
                     }
                 },
                 new AppMudule());
---
+++ src/main/java/rest/RESTService.java
@@ -0,0 +1,64 @@
+package rest;
+
+import com.google.inject.Inject;
+import model.Record;
+import storage.StorageService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+
+/**
+ * User: rgordeev
+ * Date: 15.07.14
+ * Time: 15:41
+ */
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public class RESTService
+{
+    StorageService storage;
+
+    @Inject
+    public RESTService(StorageService storage)
+    {
+        this.storage = storage;
+    }
+
+    @GET
+    public Collection<Record> list()
+    {
+        return storage.list();
+    }
+
+    @Path("{id}")
+    @GET
+    public Record user(@PathParam("id") Long id)
+    {
+        return storage.get(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Record create(Record record) {
+        return storage.add(record);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Record update(Record user) {
+        return storage.update(user);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void remove(@PathParam("id") Long id)
+    {
+        Record record = storage.get(id);
+        storage.delete(record);
+    }
+}
