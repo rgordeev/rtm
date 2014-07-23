d1d0ecefed3190e763592cba5281960beeb21422
rgordeev <roma.gordeev@gmail.com>
2014-07-23 07:36:17 +0400

    add conditions

--- src/main/webapp/js/amodule.js
+++ src/main/webapp/js/amodule.js
@@ -1,6 +1,13 @@
 (function() {
     var app = angular.module('gemStore', []);
-    var gem = {name: 'Product name', price: 200};
+
+    var gem = {
+        name: 'The Good',
+        price: 110.50,
+        canPurchase: false,
+        soldOut: false
+    };
+
     app.controller('MyController', function () {
         this.product = gem;
     });
